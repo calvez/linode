4fa238682bbdf679c5cc3790682edade42c3f15d
Jeff Baranski <jeff.baranski@outlook.com>
2016-02-28 19:45:34 -0500

    docker

--- dev.sh
+++ dev.sh
@@ -7,7 +7,22 @@
 
 # @CORE
 function get_docker {
-    curl -sSL https://get.docker.com/ubuntu/ | sudo sh
+    apt-get update
+    apt-get install apt-transport-https ca-certificates
+    sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+    rm -rf /etc/apt/sources.list.d/docker.list
+    touch /etc/apt/sources.list.d/docker.list
+    echo "deb https://apt.dockerproject.org/repo ubuntu-wily main" > /etc/apt/sources.list.d/docker.list
+    apt-get update
+    apt-get purge lxc-docker
+    apt-cache policy docker-engine
+    apt-get update
+    sudo apt-get install linux-image-extra-$(uname -r)
+    apt-get update
+    apt-get install docker-engine
+    service docker start
+    systemctl enable docker
+    apt-get upgrade docker-engine
 }
 
 function get_git {
