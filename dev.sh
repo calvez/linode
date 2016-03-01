a32f4fb124d76f3839b44aa817015635ae492129
Jeff Baranski <jeff.baranski@outlook.com>
2016-03-01 18:34:19 -0500

    clean up comments

--- dev.sh
+++ dev.sh
@@ -2,10 +2,7 @@
 #
 # @author jbaranski - https://github.com/jbaranski/linode
 #
-# @CORE - core development and production tools...
-# @PYTHON - python specific tools and packages...
 
-# @CORE
 function get_docker {
     curl -sSL https://get.docker.com/ | sudo sh
 }
